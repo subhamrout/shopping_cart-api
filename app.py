cd5b1f3b4b9589f884811952085271040cb5b820
DESKTOP-1QRE2CS\Subham Rout <subhamrout39@gmail.com>
2019-03-03 16:50:39 +0530

    second ,commit ,added REST API to access products,user authentication.

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ Created on Sun Mar  3 07:39:32 2019
 @author: Subham Rout
 """
 from flask import Flask
-from flask_restful import Api
-from flask_jwt import JWT
+from flask_restful import Resource,Api,reqparse
+from flask_jwt import JWT,jwt_required
 from security import authenticate,identity
 from resources.user import UserRegister
 from resources.bag import Bag,Print_bill,Shopping_List,Present_Bag_item
@@ -19,7 +19,14 @@ app.secret_key = "SubhamRoUtRoCkS"
 api = Api(app)
 jwt = JWT(app,authenticate,identity)
 
-        
+total = 0
+
+'''
+@app.before_first_request
+def create_all():
+    db.create_all()
+'''
+
 api.add_resource(Bag,'/<string:itemName>')        
 api.add_resource(Print_bill,"/printBill")
 api.add_resource(Shopping_List,"/shoppings")
